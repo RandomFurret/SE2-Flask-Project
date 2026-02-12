527ff87abe4ffb4d7fe64d07005bbed0f2d2f98e
John Doe <noahcwilliams42@gmail.com>
2026-02-12 11:10:42 -0500

    other pages

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
